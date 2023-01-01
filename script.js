5605049de88750cc6b8f4c8d019961d76ee93114
Noyon <sarkernoyon62@gmail.com>
2023-01-02 00:16:35 +0600

    task complete

---
+++ script.js
@@ -0,0 +1,10 @@
+// !Hide text
+function textHide() {
+  const hideText = document.getElementById("hide-text");
+  hideText.innerHTML = " ";
+}
+// !Show text
+function textShow() {
+  const showText = document.getElementById("show-text");
+  showText.innerHTML = "Good Night Everyone";
+}
